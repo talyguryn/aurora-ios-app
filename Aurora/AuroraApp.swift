eb490b2f682dcb4e1e50387c294720416071d55c
Taly <vitalik7tv@yandex.ru>
2023-12-01 19:38:28 +0300

    Initial Commit

---
+++ Aurora/AuroraApp.swift
@@ -0,0 +1,17 @@
+//
+//  AuroraApp.swift
+//  Aurora
+//
+//  Created by Taly Guryn on 01.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct AuroraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
